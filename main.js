93c38009ef59d1fc94400886ea47457dfac0ed73
lannin <67410212+a-lannin@users.noreply.github.com>
2022-08-05 11:46:00 +0800

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("touchstart", function() {}, false);
-
-var textarea = document.querySelector('textarea');
-    
-    textarea.addEventListener('input', (e) => {
-        textarea.style.height = '40px';
-        textarea.style.height = e.target.scrollHeight + 'px';
-    });
